7e76df5f9ae30958036d185bee5bbf813316b031
Joe Murray <jmurray45@gmail.com>
2024-03-06 14:25:15 -0500

    pg 282

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -101,6 +101,16 @@ var items = [ChecklistItem]()
             
 
         }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath) {
+            items.remove(at: indexPath.row)
+            
+            let indexPaths = [indexPath]
+            tableView.deleteRows(at: indexPaths, with: .automatic)
+        }
 
 
 }
