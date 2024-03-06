b62843c42dbfb203032596846b4287845d22085a
Joe Murray <jmurray45@gmail.com>
2024-03-06 14:57:30 -0500

    pg 310

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // MARK: - Actions
@@ -21,7 +27,15 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+            return nil
+        }
     
 }
