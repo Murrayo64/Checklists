95f70738d52a7cc262cbf6f199675ccf4aa76a6b
Joe Murray <jmurray45@gmail.com>
2024-03-06 14:33:53 -0500

    pg 291

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by JAM on 3/6/24.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+}
